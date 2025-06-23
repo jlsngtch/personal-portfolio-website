9e037750a46f86357d0549378849a4baf935ff23
jlsngtch <jlsngtch@gmail.com>
2025-06-23 14:52:51 +0300

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-require("dotenv").config;
-
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const contactRoute = require("./route/Contactroute");
+const contactRoute = require("./route/ContactRoute");
 
 const app = express();
 
-//creating middleware
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
 
 app.use("/", contactRoute);
@@ -21,5 +19,4 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, console.log(`server listening to port 5000`));
+app.listen(port, console.log(`server listing to port 5000 only`));
